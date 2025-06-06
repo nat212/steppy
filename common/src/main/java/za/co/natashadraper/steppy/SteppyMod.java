893d54c0862f8ece5df4588248f3c584694f128d
Natasha Draper <natasha@draper.net.za>
2025-06-06 16:17:36 +0200

    fix: sneaking not working

--- common/src/main/java/za/co/natashadraper/steppy/SteppyMod.java
+++ common/src/main/java/za/co/natashadraper/steppy/SteppyMod.java
@@ -1,9 +1,13 @@
 package za.co.natashadraper.steppy;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public final class SteppyMod {
     public static final String MOD_ID = "steppy";
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     public static void init() {
-        // Write common init code here.
+        LOGGER.info("[SteppyMod] Initialising steppy");
     }
 }
